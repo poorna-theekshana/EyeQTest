03e82eb369592b1547efc9cb365f72be62fa303a
Vihitha-Ravindu <vihitharavindu@gmail.com>
2023-09-07 23:28:41 +0530

    home page activity navigation

--- app/src/main/java/com/example/eyeqtest/HomeFragment.kt
+++ app/src/main/java/com/example/eyeqtest/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.eyeqtest
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,9 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.eyeqtest.ColorBlindTest.ColorBlindHome
+import com.example.eyeqtest.MacularDegeneration.MacularDegenerationFront
 
 class HomeFragment : Fragment() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,11 +22,36 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         val colorblindtest = view.findViewById<Button>(R.id.colorBlindTestBtn)
+        val astigmatismtest = view.findViewById<Button>(R.id.astigmatismTestBtn)
+        val masculartest = view.findViewById<Button>(R.id.mascularTestBtn)
+        val contrastsentest = view.findViewById<Button>(R.id.contrastTestBtn)
 
         colorblindtest.setOnClickListener {
             val intent = Intent(activity, ColorBlindHome::class.java)
             startActivity(intent)
         }
+
+        astigmatismtest.setOnClickListener {
+            val intent = Intent(activity, AstigmatismFront::class.java)
+            startActivity(intent)
+        }
+
+        masculartest.setOnClickListener {
+            val intent = Intent(activity, MacularDegenerationFront::class.java)
+            startActivity(intent)
+        }
+
+        contrastsentest.setOnClickListener {
+            val intent = Intent(activity, ContrastSensivityActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
         return view
     }
 
