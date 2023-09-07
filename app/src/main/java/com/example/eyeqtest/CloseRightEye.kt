b15507c3dfd65cc05bb1e035517e2039dc18e05c
Poorna Theekshana <poornatheekshana@outlook.com>
2023-09-07 17:04:34 +0530

    astigmatism activity

---
+++ app/src/main/java/com/example/eyeqtest/CloseRightEye.kt
@@ -0,0 +1,11 @@
+package com.example.eyeqtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CloseRightEye : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_close_right_eye)
+    }
+}
