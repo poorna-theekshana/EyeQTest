8f3ce83d16034f85810cc157be3bcb1dbb8078a6
Vihitha-Ravindu <vihitharavindu@gmail.com>
2023-10-12 23:46:14 +0530

    get results from database

---
+++ app/src/main/java/com/example/eyeqtest/UserAccount/MacularRecords.kt
@@ -0,0 +1,12 @@
+package com.example.eyeqtest.UserAccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eyeqtest.R
+
+class MacularRecords : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_macular_records)
+    }
+}
